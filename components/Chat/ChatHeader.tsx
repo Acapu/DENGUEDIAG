a9eb60c8fad5824eeb3ff7ed3080f43bb79b7162
acapu <asyrafmustaqimah@gmail.com>
2024-06-15 21:27:03 +0800

    add back button and restart chat button in chat header

--- components/Chat/ChatHeader.tsx
+++ components/Chat/ChatHeader.tsx
@@ -1,12 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import './style.css'
+import LeftArrow from '/leftArrow.svg'
+import Restart from '/restart.svg'
 interface props {
     title: string;
 }
 
 export default function ChatHeader({ title }: props) {
+    const navigate = useNavigate();
+
     return (
         <div className='chat-header'>
+            <button style={{ width: "fit-content", backgroundColor: "transparent" }}
+                onClick={() => navigate("/home")}
+            >
+                <img src={LeftArrow} style={{ width: "1.5em" }} />
+            </button>
             <h2> {title} </h2>
+            <button style={{ width: "fit-content", backgroundColor: "transparent" }}>
+                <img src={Restart} style={{ width: "2em" }} />
+            </button>
         </div>
     )
 }
