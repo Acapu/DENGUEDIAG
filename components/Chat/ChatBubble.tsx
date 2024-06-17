6148e2ad34ace6613135676d8a6063b514314530
acapu <asyrafmustaqimah@gmail.com>
2024-06-17 22:32:29 +0800

    fix typing animation in small device

--- components/Chat/ChatBubble.tsx
+++ components/Chat/ChatBubble.tsx
@@ -12,7 +12,7 @@ export default function ChatBody({ id="", dialog = "", type = "bot" }: props) {
     return (
         <li id={id} className={`chat-bubble-container ${type !== 'bot' ? "bubble-right" : ""}`}>
             <div className={`chat-bubble ${type === "bot" ? 'left' : 'right'} ${typeof(dialog) !== 'string' ? 'choice' : 'answer'}`}>
-                <p className={typeof(dialog) === 'string' ? 'word' : ''}>{dialog}</p>
+                <span className={typeof(dialog) === 'string' ? 'word' : ''}>{dialog}</span>
             </div>
             <img src={type === "bot" ? robot : people} alt="Avatar" className={`chat-avatar`}/>
         </li>
